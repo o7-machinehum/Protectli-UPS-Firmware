76d58fcfb3bbd601776fede26a2a11c9f631400c
Ryan Walker <ryan.cjw@gmail.com>
2023-07-12 11:18:03 -0700

    Working on the PID loop for the boost circuit.

--- rp2040-app/src/main.cpp
+++ rp2040-app/src/main.cpp
@@ -12,8 +12,15 @@
 #define STACKSIZE 16384
 
 static const struct pwm_dt_spec buck = PWM_DT_SPEC_GET(DT_ALIAS(pwm_buck));
+static const struct pwm_dt_spec boost = PWM_DT_SPEC_GET(DT_ALIAS(pwm_boost));
 
-// int main(void)
+
+// This needs to be implemented to see if we're charing or discharging
+// ie: We have power, or we don't.
+bool vin_detect(void) {
+    return true;
+}
+
 void buckboost(void)
 {
     int ret;
@@ -23,10 +30,12 @@ void buckboost(void)
 	printk("~~~ Protectli UPS ~~~\n");
 
     HwErrors hw_errors;
-    Pid pid0(12.0, 0.03, 0.0001, 0.000002);
+    Pid buck_pid(12.0, 0.03, 0.0001, 0.000002);
+    Pid boost_pid(15.0, 0.001, 0.0, 0.0);
 
     Adc adc;
 
+	k_sleep(K_MSEC(50U));
     printk("Vout: %d mV\t", adc.read_vout());
     printk("Vbat: %d mV\n", adc.read_vbat());
     printk("Iout: %d mV\t", adc.read_iout());
@@ -38,23 +47,54 @@ void buckboost(void)
 	}
 
     // Main PID Loop
+    bool first = true;
     while(true) {
         ret = hw_errors.errors();
 
-        if(!ret) {
+        if(!ret & !vin_detect()) {
+            if(first) {
+                printk("Entering Buck State\n");
+                first = false;
+            }
             actual =  adc.read_vout();
             actual = actual / 1000;
-            pid0.compute(actual);
-            drive = pid0.get_duc();
+            buck_pid.compute(actual);
+            drive = buck_pid.get_duc();
+            printk("%f\n", drive);
             if(drive < 0)
                 drive = 0;
 
+            pwm_set_dt(&boost, PERIOD, 0);
             pwm_set_dt(&buck, PERIOD, PERIOD * drive);
 
         }
-        else {
+        else if(!ret & vin_detect()) {
+            if(first) {
+                printk("Entering Boost State\n");
+                first = false;
+            }
+            actual = adc.read_vbat();
+            actual = actual / 1000;
+            if(actual > 16) {
+                pwm_set_dt(&boost, PERIOD, 0);
+                printk("Safety Trip\n");
+                while(1) {};
+            }
+            boost_pid.compute(actual);
+            drive = boost_pid.get_duc();
+            // printk("%f\n", actual);
+            if(drive < 0)
+                drive = 0;
+
             pwm_set_dt(&buck, PERIOD, 0);
-            printk("UPS In Error State: %d\n", ret);
+            pwm_set_dt(&boost, PERIOD, PERIOD * drive);
+        }
+        else {
+            if(first) {
+                printk("UPS In Error State: %d\n", ret);
+                first = false;
+            }
+            pwm_set_dt(&buck, PERIOD, 0);
 		    k_sleep(K_SECONDS(1U));
         }
 
