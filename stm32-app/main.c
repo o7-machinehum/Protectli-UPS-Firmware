96380064740cb39c9d9aecd41082d8160526facc
Ryan Walker <ryan.cjw@gmail.com>
2023-08-19 13:49:16 -0700

    UART working.

--- stm32-app/main.c
+++ stm32-app/main.c
@@ -1,6 +1,7 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/usart.h>
+#include <libopencm3/stm32/syscfg.h>
 
 #include <stdio.h>
 
@@ -12,14 +13,14 @@ void uart_out(char* data);
 
 static void clock_setup(void)
 {
-    /* Enable GPIOC clock for LED & USARTs. */
-    rcc_set_sysclk_source(RCC_CLOCK_CONFIG_HSI_PLL_64MHZ);
-    rcc_periph_clock_enable(RCC_GPIOA);
-    // rcc_periph_clock_enable(RCC_GPIOC);
+    rcc_clock_setup(&rcc_clock_config[RCC_CLOCK_CONFIG_HSI_16MHZ]);
 
-    /* Enable clocks for USART. */
-    // rcc_periph_clock_enable(RCC_USART1);
+    rcc_periph_clock_enable(RCC_GPIOA);
+    rcc_periph_clock_enable(RCC_SYSCFG);
+    rcc_periph_clock_enable(RCC_USART1);
 }
+
+// rcc_clock_setup
 
 static void usart_setup(void)
 {
@@ -39,30 +40,36 @@ static void gpio_setup(void)
 {
     gpio_mode_setup(PORT_LED, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, PIN_LED1 | PIN_LED2);
 
-    // gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO9);
-
-    // gpio_set_af(GPIOA, GPIO_AF1, GPIO9);
+    SYSCFG_CFGR1 |= SYSCFG_CFGR1_PA11_RMP;
+    gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO9);
+    gpio_set_af(GPIOA, GPIO_AF1, GPIO9);
 }
 
 void uart_out(char* data) {
     while(*data) {
-        usart_send_blocking(USART1, *data++); /* USART2: Send byte. */
+        usart_send_blocking(USART1, *data++);
     }
 }
 
 int main(void)
 {
     int i = 0;
-    // char buf[256];
+    char buf[256];
 
     clock_setup();
     gpio_setup();
-    // usart_setup();
-    // sprintf(buf, "Hello world\r\n");
-    // uart_out(buf);
+    usart_setup();
+    sprintf(buf, "Hello world\r\n");
+    uart_out(buf);
 
+    gpio_toggle(PORT_LED, PIN_LED1);
     while(1) {
-        gpio_set(PORT_LED, PIN_LED1 | PIN_LED2);
+        gpio_toggle(PORT_LED, PIN_LED1 | PIN_LED2);
+
+		for (i = 0; i < 1000000; i++) {	/* Wait a bit. */
+			__asm__("nop");
+		}
+
     }
 
     return 0;
