eb7a57ade05d4b8f08540f60ba992e1c5cb80976
Ryan Walker <ryan.cjw@gmail.com>
2023-09-28 14:03:46 -0700

    UART coming along, board needs rework.

--- esp32-app/src/main.c
+++ esp32-app/src/main.c
@@ -38,51 +38,51 @@ static int rx_buf_pos;
  */
 void serial_cb(const struct device *dev, void *user_data)
 {
-	uint8_t c;
+    uint8_t c;
 
-	if (!uart_irq_update(uart_dev)) {
-		return;
-	}
+    if (!uart_irq_update(uart_dev)) {
+        return;
+    }
 
-	if (!uart_irq_rx_ready(uart_dev)) {
-		return;
-	}
+    if (!uart_irq_rx_ready(uart_dev)) {
+        return;
+    }
 
-	/* read until FIFO empty */
-	while (uart_fifo_read(uart_dev, &c, 1) == 1) {
-		if ((c == '\n' || c == '\r') && rx_buf_pos > 0) {
-			/* terminate string */
-			rx_buf[rx_buf_pos] = '\0';
+    /* read until FIFO empty */
+    while (uart_fifo_read(uart_dev, &c, 1) == 1) {
+        if ((c == '\n' || c == '\r') && rx_buf_pos > 0) {
+            /* terminate string */
+            rx_buf[rx_buf_pos] = '\0';
 
-			/* if queue is full, message is silently dropped */
-			k_msgq_put(&uart_msgq, &rx_buf, K_NO_WAIT);
+            /* if queue is full, message is silently dropped */
+            k_msgq_put(&uart_msgq, &rx_buf, K_NO_WAIT);
 
-			/* reset the buffer (it was copied to the msgq) */
-			rx_buf_pos = 0;
-		} else if (rx_buf_pos < (sizeof(rx_buf) - 1)) {
-			rx_buf[rx_buf_pos++] = c;
-		}
-		/* else: characters beyond buffer size are dropped */
-	}
+            /* reset the buffer (it was copied to the msgq) */
+            rx_buf_pos = 0;
+        } else if (rx_buf_pos < (sizeof(rx_buf) - 1)) {
+            rx_buf[rx_buf_pos++] = c;
+        }
+        /* else: characters beyond buffer size are dropped */
+    }
 }
 
 void print_uart(char *buf)
 {
-	int msg_len = strlen(buf);
+    int msg_len = strlen(buf);
 
-	for (int i = 0; i < msg_len; i++) {
-		uart_poll_out(uart_dev, buf[i]);
-	}
+    for (int i = 0; i < msg_len; i++) {
+        uart_poll_out(uart_dev, buf[i]);
+    }
 }
 
 int main(void) {
     int ret;
     const struct device *display;
     uint16_t rows;
-	uint8_t ppt;
-	uint8_t font_width;
-	uint8_t font_height;
-	char tx_buf[MSG_SIZE];
+    uint8_t ppt;
+    uint8_t font_width;
+    uint8_t font_height;
+    char tx_buf[MSG_SIZE];
 
     printf("Starting Up!\n");
 
@@ -110,24 +110,26 @@ int main(void) {
     }
 
     if (!device_is_ready(uart_dev)) {
-		printk("UART device not found!");
-		return 0;
-	}
+        printk("UART device not found!");
+        return 0;
+    }
 
     /* configure interrupt and callback to receive data */
-	ret = uart_irq_callback_user_data_set(uart_dev, serial_cb, NULL);
+    ret = uart_irq_callback_user_data_set(uart_dev, serial_cb, NULL);
 
     if (ret < 0) {
-		if (ret == -ENOTSUP) {
-			printk("Interrupt-driven UART API support not enabled\n");
-		} else if (ret == -ENOSYS) {
-			printk("UART device does not support interrupt-driven API\n");
-		} else {
-			printk("Error setting UART callback: %d\n", ret);
-		}
-		return 0;
-	}
-	uart_irq_rx_enable(uart_dev);
+        if (ret == -ENOTSUP) {
+            printk("Interrupt-driven UART API support not enabled\n");
+        } else if (ret == -ENOSYS) {
+            printk("UART device does not support interrupt-driven API\n");
+        } else {
+            printk("Error setting UART callback: %d\n", ret);
+        }
+        return 0;
+    }
+
+    uart_irq_rx_enable(uart_dev);
+    // print_uart("Testing");
 
     cfb_framebuffer_clear(display, true);
     display_blanking_off(display);
@@ -162,11 +164,10 @@ int main(void) {
     while (1) {
         gpio_pin_toggle_dt(&led);
 
-	    if (!k_msgq_get(&uart_msgq, &tx_buf, K_NO_WAIT)) {
-			printk("Got some Data: %s\n", tx_buf);
+        if (!k_msgq_get(&uart_msgq, &tx_buf, K_NO_WAIT)) {
+            printk("Got some Data: %s\n", tx_buf);
         }
 
-        k_msleep(SLEEP_TIME_MS);
     }
     return 0;
 }
--- rp2040-app/src/main.cpp
+++ rp2040-app/src/main.cpp
@@ -2,6 +2,9 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/pwm.h>
+#include <zephyr/drivers/uart.h>
+#include <string.h>
+#include <stdio.h>
 
 #include "hw_errors.h"
 #include "pid.h"
@@ -10,6 +13,9 @@
 
 #define PERIOD PWM_NSEC(2000) // 500Khz
 #define STACKSIZE 16384
+
+#define MSG_SIZE 32
+K_MSGQ_DEFINE(uart_msgq, MSG_SIZE, 10, 4);
 
 static const struct pwm_dt_spec pwm =
     PWM_DT_SPEC_GET(DT_ALIAS(pwm_0));
@@ -26,17 +32,20 @@ static const struct gpio_dt_spec vin_detect =
 static const struct gpio_dt_spec pack_boot =
     GPIO_DT_SPEC_GET(DT_ALIAS(pack_boot), gpios);
 
-static const struct gpio_dt_spec gpio0 =
-    GPIO_DT_SPEC_GET(DT_ALIAS(gpio_0), gpios);
-
-static const struct gpio_dt_spec gpio1 =
-    GPIO_DT_SPEC_GET(DT_ALIAS(gpio_1), gpios);
+// static const struct gpio_dt_spec gpio0 =
+//     GPIO_DT_SPEC_GET(DT_ALIAS(gpio_0), gpios);
+//
+// static const struct gpio_dt_spec gpio1 =
+//     GPIO_DT_SPEC_GET(DT_ALIAS(gpio_1), gpios);
 
 static const struct gpio_dt_spec gpio2 =
     GPIO_DT_SPEC_GET(DT_ALIAS(gpio_2), gpios);
 
 static const struct gpio_dt_spec gpio3 =
     GPIO_DT_SPEC_GET(DT_ALIAS(gpio_3), gpios);
+
+static const struct device *const uart_dev
+    = DEVICE_DT_GET(DT_CHOSEN(zephyr_mcu_uart));
 
 void print_voltages(Adc adc, float drive, Bump pid) {
     printk("Vout : %d mV\t", adc.read_vout());
@@ -45,6 +54,53 @@ void print_voltages(Adc adc, float drive, Bump pid) {
     printk("Ibat : %d mA\t", adc.read_ibat());
     printk("Boost: %f  \t", pid.get_duc());
     printk("Drve : %f\n", drive);
+}
+
+/* receive buffer used in UART ISR callback */
+static char rx_buf[MSG_SIZE];
+static int rx_buf_pos;
+
+/*
+ * Read characters from UART until line end is detected. Afterwards push the
+ * data to the message queue.
+ */
+void serial_cb(const struct device *dev, void *user_data)
+{
+	uint8_t c;
+
+	if (!uart_irq_update(uart_dev)) {
+		return;
+	}
+
+	if (!uart_irq_rx_ready(uart_dev)) {
+		return;
+	}
+
+	/* read until FIFO empty */
+	while (uart_fifo_read(uart_dev, &c, 1) == 1) {
+		if ((c == '\n' || c == '\r') && rx_buf_pos > 0) {
+			/* terminate string */
+			rx_buf[rx_buf_pos] = '\0';
+
+			/* if queue is full, message is silently dropped */
+			k_msgq_put(&uart_msgq, &rx_buf, K_NO_WAIT);
+
+			/* reset the buffer (it was copied to the msgq) */
+			rx_buf_pos = 0;
+		} else if (rx_buf_pos < (sizeof(rx_buf) - 1)) {
+			rx_buf[rx_buf_pos++] = c;
+		}
+		/* else: characters beyond buffer size are dropped */
+	}
+}
+
+void print_uart(char *buf)
+{
+    int msg_len = strlen(buf);
+
+    for (int i = 0; i < msg_len; i++) {
+        uart_poll_out(uart_dev, buf[i]);
+    }
 }
 
 enum State {
@@ -66,13 +122,13 @@ void buckboost(void)
     Pid buck_pid(12.0, 0.03, 0.0001, 0.0);
     // Pid boost_pid(500.0, 0.0007, 0.000001, 0.0); // 500mA current target
     Bump boost_pid(500.0, 0.00000001);
-    gpio_pin_configure_dt(&pwm_en, GPIO_OUTPUT);
+    gpio_pin_configure_dt(&pwm_en, GPIO_OUTPUT_INACTIVE);
     gpio_pin_configure_dt(&pack_boot, GPIO_OUTPUT);
     gpio_pin_configure_dt(&pwm_skip, GPIO_OUTPUT_ACTIVE);
     gpio_pin_configure_dt(&vin_detect, GPIO_INPUT);
 
-    gpio_pin_configure_dt(&gpio0, GPIO_DISCONNECTED);
-    gpio_pin_configure_dt(&gpio1, GPIO_DISCONNECTED);
+    // gpio_pin_configure_dt(&gpio0, GPIO_DISCONNECTED);
+    // gpio_pin_configure_dt(&gpio1, GPIO_DISCONNECTED);
     gpio_pin_configure_dt(&gpio2, GPIO_DISCONNECTED);
     gpio_pin_configure_dt(&gpio3, GPIO_DISCONNECTED);
 
@@ -86,8 +142,31 @@ void buckboost(void)
                pwm.dev->name);
     }
 
+    if (!device_is_ready(uart_dev)) {
+		printk("UART device not found!");
+	}
+
+    /* configure interrupt and callback to receive data */
+	ret = uart_irq_callback_user_data_set(uart_dev, serial_cb, NULL);
+
+    if (ret < 0) {
+		if (ret == -ENOTSUP) {
+			printk("Interrupt-driven UART API support not enabled\n");
+		} else if (ret == -ENOSYS) {
+			printk("UART device does not support interrupt-driven API\n");
+		} else {
+			printk("Error setting UART callback: %d\n", ret);
+		}
+	}
+
+	uart_irq_rx_enable(uart_dev);
+
     state = NONE;
     int countdown = 1000;
+
+    char uartbuf[32] = {};
+    sprintf(uartbuf, "Hello \r\n");
+    print_uart(uartbuf);
 
     while(true) {
         if(!countdown--) {
