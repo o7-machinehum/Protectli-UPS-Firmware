46ad2670767275e8cccb27a3f69041fe66e9be30
Walker <machinehum@Walkers-MacBook-Pro.local>
2023-10-11 17:03:36 -0700

    Worked on the test code and header.

--- common/msg.h
+++ common/msg.h
@@ -2,48 +2,50 @@
 #include <string.h>
 
 struct Msg {
-    uint8_t v1;
-    uint8_t v2;
-    // uint8_t v3;
-    // uint8_t v4;
+	uint8_t v1;
+	uint8_t v2;
+	uint8_t pad[2];
+	uint16_t v3;
+	uint16_t v4;
+	uint32_t v5;
 };
 
 // https://blog.mbedded.ninja/programming/serialization-formats/consistent-overhead-byte-stuffing-cobs/
-uint16_t msg_encode(struct Msg msg, char *buf) {
-    uint8_t *ptr = (uint8_t*)(&msg);
-    int k = 0;
-    uint8_t len = sizeof(struct Msg);
+uint16_t msg_encode(struct Msg msg, char *buf)
+{
+	uint8_t *ptr = (uint8_t *)(&msg);
+	int k = 0;
+	uint8_t len = sizeof(struct Msg);
 
-    buf[len+2] = 0; // End Byte
-    int i = len;
-    while(i--) {
-        k++;
-        if(ptr[i] == 0) {
-            buf[i+1] = k;
-            k = 0;
-        }
-        else {
-            buf[i+1] = ptr[i];
-        }
-    }
-    k++;
-    buf[0] = k; // First byte
+	buf[len + 2] = 0; // End Byte
+	int i = len;
+	while (i--) {
+		k++;
+		if (ptr[i] == 0) {
+			buf[i + 1] = k;
+			k = 0;
+		} else {
+			buf[i + 1] = ptr[i];
+		}
+	}
+	k++;
+	buf[0] = k; // First byte
 
-    return len + 2;
+	return len + 2;
 }
 
-uint16_t msg_decode(char *buf, struct Msg msg) {
-    uint8_t *ptr = (uint8_t*)(&msg);
-    uint8_t len = sizeof(struct Msg);
+uint16_t msg_decode(char *buf, struct Msg msg)
+{
+	uint8_t *ptr = (uint8_t *)(&msg);
+	uint8_t len = sizeof(struct Msg);
 
-    int i = 0;
-    while(buf[i]) {
-        size_t temp = buf[i];
-        buf[i] = 0;
-        i += temp;
-    }
+	int i = 0;
+	while (buf[i]) {
+		size_t temp = buf[i];
+		buf[i] = 0;
+		i += temp;
+	}
 
-    memcpy(buf, (uint8_t*)(&msg), len);
-    return len;
+	memcpy(buf, (uint8_t *)(&msg), len);
+	return len;
 }
-
--- common/test.c
+++ common/test.c
@@ -1,19 +1,23 @@
 #include "stdint.h"
 #include "msg.h"
 
-int main() {
-    char buff[32] = {};
-    uint16_t len  = 0;
+int main()
+{
+	char buff[256] = {};
+	uint16_t len = 0;
 
-    struct Msg m1 = {
-        .v1 = 0,
-        .v2 = 1,
-        // .v3 = 0,
-        // .v4 = 1
-    };
+	struct Msg m1 = {
+		.v1 = 0,
+		.v2 = 200,
+		.v3 = 19209,
+		.v4 = 38021,
+		.v5 = 8903829,
+	};
 
-    struct Msg m2 = {0};
+	struct Msg m2 = {0};
 
-    len = msg_encode(m1, buff);
-    len = msg_decode(buff, m2);
+	len = msg_encode(m1, buff);
+	len = msg_decode(buff, m2);
+
+	printf("%d, %d, %d, %d, %d\n", m2.v1, m2.v2, m2.v3, m2.v4, m2.v5);
 }
