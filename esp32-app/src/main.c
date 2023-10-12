765523a87f4d4a4374eba0f66cc9d898ea311564
Walker <machinehum@Walkers-MacBook-Pro.local>
2023-10-11 17:02:35 -0700

    Added clang formatting. Formatting the esp32 code.

--- esp32-app/src/main.c
+++ esp32-app/src/main.c
@@ -1,33 +1,28 @@
-#include <zephyr/kernel.h>
+#include <stdio.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/spi.h>
 #include <zephyr/drivers/uart.h>
-#include <stdio.h>
+#include <zephyr/kernel.h>
 
 #include "../../common/msg.h"
 #include "screen.h"
 
-#define SLEEP_TIME_MS   5000
-#define LED0_NODE DT_ALIAS(led0)
+#define SLEEP_TIME_MS 5000
+#define LED0_NODE     DT_ALIAS(led0)
 
 #define MSG_SIZE 32
 
 K_MSGQ_DEFINE(uart_msgq, MSG_SIZE, 10, 4);
 
-static struct k_poll_signal spi_slave_done_sig
-    = K_POLL_SIGNAL_INITIALIZER(spi_slave_done_sig);
+static struct k_poll_signal spi_slave_done_sig = K_POLL_SIGNAL_INITIALIZER(spi_slave_done_sig);
 
-static const struct gpio_dt_spec led
-    = GPIO_DT_SPEC_GET(DT_NODELABEL(led0), gpios);
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_NODELABEL(led0), gpios);
 
-static const struct gpio_dt_spec relay1
-    = GPIO_DT_SPEC_GET(DT_NODELABEL(relay1), gpios);
+static const struct gpio_dt_spec relay1 = GPIO_DT_SPEC_GET(DT_NODELABEL(relay1), gpios);
 
-static const struct gpio_dt_spec relay2
-    = GPIO_DT_SPEC_GET(DT_NODELABEL(relay2), gpios);
+static const struct gpio_dt_spec relay2 = GPIO_DT_SPEC_GET(DT_NODELABEL(relay2), gpios);
 
-static const struct device *const uart_dev
-    = DEVICE_DT_GET(DT_CHOSEN(zephyr_mcu_uart));
+static const struct device *const uart_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_mcu_uart));
 
 /* receive buffer used in UART ISR callback */
 static char rx_buf[MSG_SIZE];
@@ -41,101 +36,101 @@ struct Msg msg = {};
  */
 void serial_cb(const struct device *dev, void *user_data)
 {
-    uint8_t c;
+	uint8_t c;
 
-    if (!uart_irq_update(uart_dev)) {
-        return;
-    }
+	if (!uart_irq_update(uart_dev)) {
+		return;
+	}
 
-    if (!uart_irq_rx_ready(uart_dev)) {
-        return;
-    }
-    while (uart_fifo_read(uart_dev, &c, 1) == 1) {
-        printk("int: %d\n", c);
-        *((uint8_t*)(&msg) + rx_buf_pos++) = c;
-        if(rx_buf_pos >= sizeof(struct Msg)) {
-            // printk("thing: %d\n", msg.voltage);
-            // printk("thing: %d\n", msg.current);
-        }
-    }
+	if (!uart_irq_rx_ready(uart_dev)) {
+		return;
+	}
+	while (uart_fifo_read(uart_dev, &c, 1) == 1) {
+		printk("int: %d\n", c);
+		*((uint8_t *)(&msg) + rx_buf_pos++) = c;
+		if (rx_buf_pos >= sizeof(struct Msg)) {
+			// printk("thing: %d\n", msg.voltage);
+			// printk("thing: %d\n", msg.current);
+		}
+	}
 
-    /* read until FIFO empty */
-    // while (uart_fifo_read(uart_dev, &c, 1) == 1) {
-    //     printk("%c", c);
-    //     if ((c == '\n' || c == '\r') && rx_buf_pos > 0) {
-    //         /* terminate string */
-    //         rx_buf[rx_buf_pos] = '\0';
+	/* read until FIFO empty */
+	// while (uart_fifo_read(uart_dev, &c, 1) == 1) {
+	//     printk("%c", c);
+	//     if ((c == '\n' || c == '\r') && rx_buf_pos > 0) {
+	//         /* terminate string */
+	//         rx_buf[rx_buf_pos] = '\0';
 
-    //         /* if queue is full, message is silently dropped */
-    //         k_msgq_put(&uart_msgq, &rx_buf, K_NO_WAIT);
+	//         /* if queue is full, message is silently dropped */
+	//         k_msgq_put(&uart_msgq, &rx_buf, K_NO_WAIT);
 
-    //         printk("rx_buf: %s\n", rx_buf);
+	//         printk("rx_buf: %s\n", rx_buf);
 
-    //         /* reset the buffer (it was copied to the msgq) */
-    //         rx_buf_pos = 0;
-    //     } else if (rx_buf_pos < (sizeof(rx_buf) - 1)) {
-    //         printk("Add \n");
-    //         rx_buf[rx_buf_pos++] = c;
-    //     }
-    //     /* else: characters beyond buffer size are dropped */
-    // }
+	//         /* reset the buffer (it was copied to the msgq) */
+	//         rx_buf_pos = 0;
+	//     } else if (rx_buf_pos < (sizeof(rx_buf) - 1)) {
+	//         printk("Add \n");
+	//         rx_buf[rx_buf_pos++] = c;
+	//     }
+	//     /* else: characters beyond buffer size are dropped */
+	// }
 }
 
 void print_uart(char *buf)
 {
-    int msg_len = strlen(buf);
+	int msg_len = strlen(buf);
 
-    for (int i = 0; i < msg_len; i++) {
-        uart_poll_out(uart_dev, buf[i]);
-    }
+	for (int i = 0; i < msg_len; i++) {
+		uart_poll_out(uart_dev, buf[i]);
+	}
 }
 
-int main(void) {
-    int ret;
-    char tx_buf[MSG_SIZE];
+int main(void)
+{
+	int ret;
+	char tx_buf[MSG_SIZE];
 
-    printf("Starting Up!\n");
+	printf("Starting Up!\n");
 
-    if (!gpio_is_ready_dt(&led)) {
-        return 0;
-    }
+	if (!gpio_is_ready_dt(&led)) {
+		return 0;
+	}
 
 	screen_init();
-    gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
-    gpio_pin_configure_dt(&relay1, GPIO_OUTPUT_ACTIVE);
-    
-    if (!device_is_ready(uart_dev)) {
-        printk("UART device not found!");
-        return 0;
-    }
+	gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
+	gpio_pin_configure_dt(&relay1, GPIO_OUTPUT_ACTIVE);
 
-    /* configure interrupt and callback to receive data */
-    ret = uart_irq_callback_user_data_set(uart_dev, serial_cb, NULL);
+	if (!device_is_ready(uart_dev)) {
+		printk("UART device not found!");
+		return 0;
+	}
 
-    if (ret < 0) {
-        if (ret == -ENOTSUP) {
-            printk("Interrupt-driven UART API support not enabled\n");
-        } else if (ret == -ENOSYS) {
-            printk("UART device does not support interrupt-driven API\n");
-        } else {
-            printk("Error setting UART callback: %d\n", ret);
-        }
-        return 0;
-    }
+	/* configure interrupt and callback to receive data */
+	ret = uart_irq_callback_user_data_set(uart_dev, serial_cb, NULL);
 
-    uart_irq_rx_enable(uart_dev);
+	if (ret < 0) {
+		if (ret == -ENOTSUP) {
+			printk("Interrupt-driven UART API support not enabled\n");
+		} else if (ret == -ENOSYS) {
+			printk("UART device does not support interrupt-driven API\n");
+		} else {
+			printk("Error setting UART callback: %d\n", ret);
+		}
+		return 0;
+	}
+
+	uart_irq_rx_enable(uart_dev);
 	screen_draw();
-    // print_uart("Testing");
-    
-    while (1) {
-        gpio_pin_toggle_dt(&led);
+	// print_uart("Testing");
 
-        if (!k_msgq_get(&uart_msgq, &tx_buf, K_NO_WAIT)) {
-            printk("Data: %s", tx_buf);
-        }
+	while (1) {
+		gpio_pin_toggle_dt(&led);
 
-        k_sleep(K_MSEC(100U));
+		if (!k_msgq_get(&uart_msgq, &tx_buf, K_NO_WAIT)) {
+			printk("Data: %s", tx_buf);
+		}
 
-    }
-    return 0;
+		k_sleep(K_MSEC(100U));
+	}
+	return 0;
 }
--- esp32-app/src/screen.c
+++ esp32-app/src/screen.c
@@ -4,68 +4,66 @@
 
 const struct device *display;
 
-int screen_init() {
+int screen_init()
+{
 	display = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
 
-    if (!device_is_ready(display)) {
-        printf("Device %s not ready\n", display->name);
-        return 0;
-    }
+	if (!device_is_ready(display)) {
+		printf("Device %s not ready\n", display->name);
+		return 0;
+	}
 
-    if (display_set_pixel_format(display, PIXEL_FORMAT_MONO10) != 0) {
-        printf("Failed to set required pixel format\n");
-        return 0;
-    }
+	if (display_set_pixel_format(display, PIXEL_FORMAT_MONO10) != 0) {
+		printf("Failed to set required pixel format\n");
+		return 0;
+	}
 
-    if (cfb_framebuffer_init(display)) {
-        printf("Framebuffer initialization failed!\n");
-        return 0;
-    }
+	if (cfb_framebuffer_init(display)) {
+		printf("Framebuffer initialization failed!\n");
+		return 0;
+	}
 
 	return 0;
-
 }
 
-int screen_set_font() {
-
+int screen_set_font()
+{
 }
 
-int screen_draw() {
+int screen_draw()
+{
 	uint16_t rows;
-    uint8_t ppt;
-    uint8_t font_width;
-    uint8_t font_height;
+	uint8_t ppt;
+	uint8_t font_width;
+	uint8_t font_height;
 
 	cfb_framebuffer_clear(display, true);
-    display_blanking_off(display);
-    rows = cfb_get_display_parameter(display, CFB_DISPLAY_ROWS);
-    ppt = cfb_get_display_parameter(display, CFB_DISPLAY_PPT);
+	display_blanking_off(display);
+	rows = cfb_get_display_parameter(display, CFB_DISPLAY_ROWS);
+	ppt = cfb_get_display_parameter(display, CFB_DISPLAY_PPT);
 
-    for (int idx = 0; idx < 42; idx++) {
-        if (cfb_get_font_size(display, idx, &font_width, &font_height)) {
-            break;
-        }
-		if(font_height == 16)
-        	cfb_framebuffer_set_font(display, idx);
+	for (int idx = 0; idx < 42; idx++) {
+		if (cfb_get_font_size(display, idx, &font_width, &font_height)) {
+			break;
+		}
+		if (font_height == 16) {
+			cfb_framebuffer_set_font(display, idx);
+		}
 
-        printf("font width %d, font height %d\n",
-               font_width, font_height);
-    }
+		printf("font width %d, font height %d\n", font_width, font_height);
+	}
 
-    printf("x_res %d, y_res %d, ppt %d, rows %d, cols %d\n",
-           cfb_get_display_parameter(display, CFB_DISPLAY_WIDTH),
-           cfb_get_display_parameter(display, CFB_DISPLAY_HEIGH),
-           ppt,
-           rows,
-           cfb_get_display_parameter(display, CFB_DISPLAY_COLS));
+	printf("x_res %d, y_res %d, ppt %d, rows %d, cols %d\n",
+	       cfb_get_display_parameter(display, CFB_DISPLAY_WIDTH),
+	       cfb_get_display_parameter(display, CFB_DISPLAY_HEIGH), ppt, rows,
+	       cfb_get_display_parameter(display, CFB_DISPLAY_COLS));
 
-    cfb_framebuffer_clear(display, true);
+	cfb_framebuffer_clear(display, true);
 
-    cfb_draw_text(display, "Hello_new", 10, 10);
-    // cfb_draw_rect(display, &start, &end);
+	cfb_draw_text(display, "Hello_new", 10, 10);
+	// cfb_draw_rect(display, &start, &end);
 
-    cfb_framebuffer_finalize(display);
-    // cfb_invert_area(display, 0, 0, 100, 64);
-    // printk("%d", display->buf[0]);
-
+	cfb_framebuffer_finalize(display);
+	// cfb_invert_area(display, 0, 0, 100, 64);
+	// printk("%d", display->buf[0]);
 }
