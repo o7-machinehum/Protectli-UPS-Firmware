710d0fc56cc58f8f80de368e66f46c2ad78a8e2f
Ryan Walker <ryan.cjw@gmail.com>
2023-10-12 13:03:29 -0700

    COBS enconding working.

--- common/msg.h
+++ common/msg.h
@@ -8,44 +8,46 @@ struct Msg {
 	uint16_t v3;
 	uint16_t v4;
 	uint32_t v5;
+	float v6;
 };
 
 // https://blog.mbedded.ninja/programming/serialization-formats/consistent-overhead-byte-stuffing-cobs/
-uint16_t msg_encode(struct Msg msg, char *buf)
+// Please note this cobs encoding and decoding is quite limited. If the packet is over a certain side
+// there will be issues, and it doesn't check buffer lengths.
+uint16_t msg_cobs_encode(struct Msg msg, char *buf)
 {
 	uint8_t *ptr = (uint8_t *)(&msg);
-	int k = 0;
+	int k = 1;
 	uint8_t len = sizeof(struct Msg);
 
-	buf[len + 2] = 0; // End Byte
+	buf[len + 1] = 0; // End Byte
 	int i = len;
 	while (i--) {
-		k++;
 		if (ptr[i] == 0) {
 			buf[i + 1] = k;
-			k = 0;
+			k = 1;
 		} else {
 			buf[i + 1] = ptr[i];
+			k++;
 		}
 	}
-	k++;
 	buf[0] = k; // First byte
 
 	return len + 2;
 }
 
-uint16_t msg_decode(char *buf, struct Msg msg)
+uint16_t msg_cobs_decode(char *buf, struct Msg *msg)
 {
 	uint8_t *ptr = (uint8_t *)(&msg);
 	uint8_t len = sizeof(struct Msg);
 
-	int i = 0;
-	while (buf[i]) {
-		size_t temp = buf[i];
+	int offset = 0;
+	for (int i = 0; buf[i]; i++) {
+		offset = buf[i];
 		buf[i] = 0;
-		i += temp;
+		i += offset - 1;
 	}
 
-	memcpy(buf, (uint8_t *)(&msg), len);
+	memcpy((uint8_t *)(msg), buf + 1, len);
 	return len;
 }
--- common/test.c
+++ common/test.c
@@ -6,18 +6,12 @@ int main()
 	char buff[256] = {};
 	uint16_t len = 0;
 
-	struct Msg m1 = {
-		.v1 = 0,
-		.v2 = 200,
-		.v3 = 19209,
-		.v4 = 38021,
-		.v5 = 8903829,
-	};
+	struct Msg m1 = {.v1 = 0, .v2 = 200, .v3 = 19209, .v4 = 0, .v5 = 8903829, .v6 = 10.3243};
 
 	struct Msg m2 = {0};
 
-	len = msg_encode(m1, buff);
-	len = msg_decode(buff, m2);
+	len = msg_cobs_encode(m1, buff);
+	len = msg_cobs_decode(buff, &m2);
 
-	printf("%d, %d, %d, %d, %d\n", m2.v1, m2.v2, m2.v3, m2.v4, m2.v5);
+	printf("%d, %d, %d, %d, %d, %f\n", m2.v1, m2.v2, m2.v3, m2.v4, m2.v5, m2.v6);
 }
