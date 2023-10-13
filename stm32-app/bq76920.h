7fdd32e55e5aeda04f55a861a01243a4157bb7d6
Ryan Walker <ryan.cjw@gmail.com>
2023-10-12 21:06:08 -0700

    clang-format

--- stm32-app/bq76920.h
+++ stm32-app/bq76920.h
@@ -5,14 +5,14 @@
 
 #define SYS_CTRL1 0x04
 #define SYS_CTRL2 0x05
-#define PROTECT1 0x06
+#define PROTECT1  0x06
 
-#define OV_TRIP 0x09
-#define UV_TRIP 0x0A
-#define CC_CFG 0x0B
+#define OV_TRIP   0x09
+#define UV_TRIP   0x0A
+#define CC_CFG    0x0B
 #define ADCOFFSET 0x51
-#define ADCGAIN1 0x50
-#define ADCGAIN2 0x59
+#define ADCGAIN1  0x50
+#define ADCGAIN2  0x59
 
 #define VC1_HI 0x0C
 #define VC1_LO 0x0D
@@ -33,72 +33,81 @@ void bq76920_clear_faults(void);
 uint16_t bq76920_read_cell_v(uint8_t call);
 void bq76920_shutdown(void);
 
-void bq76920_write_reg(uint8_t reg, uint8_t val) {
-    uint8_t dat[2];
-    dat[0] = reg;
-    dat[1] = val;
+void bq76920_write_reg(uint8_t reg, uint8_t val)
+{
+	uint8_t dat[2];
+	dat[0] = reg;
+	dat[1] = val;
 
-    i2c_transfer7(I2C1, I2C_ADDR, dat, 2, 0, 0);
+	i2c_transfer7(I2C1, I2C_ADDR, dat, 2, 0, 0);
 }
 
-uint8_t bq76920_read_reg(uint8_t reg) {
-    uint8_t dat;
+uint8_t bq76920_read_reg(uint8_t reg)
+{
+	uint8_t dat;
 
-    i2c_transfer7(I2C1, I2C_ADDR, &reg, 1, &dat, 1);
-    return dat;
+	i2c_transfer7(I2C1, I2C_ADDR, &reg, 1, &dat, 1);
+	return dat;
 }
 
-void bq76920_set_uv(int voltage_mv) {
-    uint16_t uv_trip = (((voltage_mv - adc_offset) * 1000 / adc_gain) >> 4)
-        & 0x00FF;
-    uv_trip += 1;
+void bq76920_set_uv(int voltage_mv)
+{
+	uint16_t uv_trip =
+		(((voltage_mv - adc_offset) * 1000 / adc_gain) >> 4) & 0x00FF;
+	uv_trip += 1;
 
-    bq76920_write_reg(UV_TRIP, uv_trip);
+	bq76920_write_reg(UV_TRIP, uv_trip);
 }
 
-void bq76920_set_ov(int voltage_mv) {
-    uint16_t ov_trip = (((voltage_mv - adc_offset) * 1000 / adc_gain) >> 4)
-        & 0x00FF;
+void bq76920_set_ov(int voltage_mv)
+{
+	uint16_t ov_trip =
+		(((voltage_mv - adc_offset) * 1000 / adc_gain) >> 4) & 0x00FF;
 
-    bq76920_write_reg(OV_TRIP, ov_trip);
+	bq76920_write_reg(OV_TRIP, ov_trip);
 }
 
-void bq76920_output_enable(void) {
-    bq76920_write_reg(SYS_CTRL2, 0b00000011);
+void bq76920_output_enable(void)
+{
+	bq76920_write_reg(SYS_CTRL2, 0b00000011);
 }
 
-void bq76920_init() {
-    adc_offset = (int8_t) bq76920_read_reg(ADCOFFSET);
+void bq76920_init()
+{
+	adc_offset = (int8_t)bq76920_read_reg(ADCOFFSET);
 
-    adc_gain = 365 + (((bq76920_read_reg(ADCGAIN1) & 0b00001100) << 1) |
-      ((bq76920_read_reg(ADCGAIN2) & 0b11100000) >> 5));
+	adc_gain = 365 + (((bq76920_read_reg(ADCGAIN1) & 0b00001100) << 1) |
+			  ((bq76920_read_reg(ADCGAIN2) & 0b11100000) >> 5));
 
-    bq76920_set_uv(2900); // 3V / Cell
-    bq76920_set_ov(4200); // 4.2V / Cell
-    bq76920_write_reg(SYS_CTRL1, 0b00010000); // ADC_EN = 1
+	bq76920_set_uv(2900);                     // 3V / Cell
+	bq76920_set_ov(4200);                     // 4.2V / Cell
+	bq76920_write_reg(SYS_CTRL1, 0b00010000); // ADC_EN = 1
 
-    // bq76920_write_reg(CELLBAL1, 0b00010111);
-    bq76920_write_reg(CELLBAL1, 0x00);
+	// bq76920_write_reg(CELLBAL1, 0b00010111);
+	bq76920_write_reg(CELLBAL1, 0x00);
 
-    bq76920_write_reg(PROTECT1, 0b00000001);
-    // PROTECT2 Left at default settings (8A Over Current)
+	bq76920_write_reg(PROTECT1, 0b00000001);
+	// PROTECT2 Left at default settings (8A Over Current)
 }
 
-void bq76920_clear_faults(void) {
-    bq76920_write_reg(SYS_STAT, 0b00111111);
+void bq76920_clear_faults(void)
+{
+	bq76920_write_reg(SYS_STAT, 0b00111111);
 }
 
-void bq76920_shutdown(void) {
-    bq76920_write_reg(SYS_CTRL1, 0b00000001);
-    bq76920_write_reg(SYS_CTRL1, 0b00000010);
+void bq76920_shutdown(void)
+{
+	bq76920_write_reg(SYS_CTRL1, 0b00000001);
+	bq76920_write_reg(SYS_CTRL1, 0b00000010);
 }
 
-uint16_t bq76920_read_cell_v(uint8_t cell) {
-    uint16_t v = 0;
-    cell = cell * 2;
+uint16_t bq76920_read_cell_v(uint8_t cell)
+{
+	uint16_t v = 0;
+	cell = cell * 2;
 
-    v = bq76920_read_reg(VC1_LO+cell);
-    v |= (bq76920_read_reg(VC1_HI+cell) << 8);
+	v = bq76920_read_reg(VC1_LO + cell);
+	v |= (bq76920_read_reg(VC1_HI + cell) << 8);
 
-    return (v*adc_gain / 1000) + adc_offset;
+	return (v * adc_gain / 1000) + adc_offset;
 }
--- stm32-app/main.c
+++ stm32-app/main.c
@@ -12,151 +12,157 @@
 #define PIN_LED1 GPIO5
 #define PIN_LED2 GPIO7
 
-void uart_out(char* data);
+void uart_out(char *data);
 void hard_fault(void);
 void delay(int t);
 uint8_t check_faults(uint8_t *fault_counter);
 
-static void clock_setup(void) {
-    rcc_clock_setup(&rcc_clock_config[RCC_CLOCK_CONFIG_HSI_16MHZ]);
+static void clock_setup(void)
+{
+	rcc_clock_setup(&rcc_clock_config[RCC_CLOCK_CONFIG_HSI_16MHZ]);
 
-    rcc_periph_clock_enable(RCC_GPIOA);
-    rcc_periph_clock_enable(RCC_GPIOB);
-    rcc_periph_clock_enable(RCC_SYSCFG);
-    rcc_periph_clock_enable(RCC_USART1);
-    rcc_periph_clock_enable(RCC_I2C1);
+	rcc_periph_clock_enable(RCC_GPIOA);
+	rcc_periph_clock_enable(RCC_GPIOB);
+	rcc_periph_clock_enable(RCC_SYSCFG);
+	rcc_periph_clock_enable(RCC_USART1);
+	rcc_periph_clock_enable(RCC_I2C1);
 }
 
-static void usart_setup(void) {
-    SYSCFG_CFGR1 |= SYSCFG_CFGR1_PA11_RMP;
-    gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO9);
-    gpio_set_af(GPIOA, GPIO_AF1, GPIO9);
+static void usart_setup(void)
+{
+	SYSCFG_CFGR1 |= SYSCFG_CFGR1_PA11_RMP;
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO9);
+	gpio_set_af(GPIOA, GPIO_AF1, GPIO9);
 
-    /* Setup USART parameters. */
-    usart_set_baudrate(USART1, 115200);
-    usart_set_databits(USART1, 8);
-    usart_set_parity(USART1, USART_PARITY_NONE);
-    usart_set_stopbits(USART1, USART_CR2_STOPBITS_1);
-    usart_set_mode(USART1, USART_MODE_TX);
-    usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
+	/* Setup USART parameters. */
+	usart_set_baudrate(USART1, 115200);
+	usart_set_databits(USART1, 8);
+	usart_set_parity(USART1, USART_PARITY_NONE);
+	usart_set_stopbits(USART1, USART_CR2_STOPBITS_1);
+	usart_set_mode(USART1, USART_MODE_TX);
+	usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
 
-    /* Finally enable the USART. */
-    usart_enable(USART1);
+	/* Finally enable the USART. */
+	usart_enable(USART1);
 }
 
-static void i2c_setup(void) {
-    rcc_periph_reset_pulse(RST_I2C1);
+static void i2c_setup(void)
+{
+	rcc_periph_reset_pulse(RST_I2C1);
 
-    gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO6 | GPIO7);
-    gpio_set_af(GPIOB, GPIO_AF6, GPIO6 | GPIO7);
-    gpio_set_output_options(GPIOB, GPIO_OTYPE_OD, GPIO_OSPEED_HIGH, GPIO6 | GPIO7);
+	gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO6 | GPIO7);
+	gpio_set_af(GPIOB, GPIO_AF6, GPIO6 | GPIO7);
+	gpio_set_output_options(GPIOB, GPIO_OTYPE_OD, GPIO_OSPEED_HIGH,
+				GPIO6 | GPIO7);
 
-    i2c_peripheral_disable(I2C1);
-    i2c_disable_stretching(I2C1);
-    i2c_enable_analog_filter(I2C1);
-    i2c_set_digital_filter(I2C1, 5);
-    i2c_set_speed(I2C1, i2c_speed_sm_100k, 16);
-    i2c_set_7bit_addr_mode(I2C1);
-    i2c_peripheral_enable(I2C1);
+	i2c_peripheral_disable(I2C1);
+	i2c_disable_stretching(I2C1);
+	i2c_enable_analog_filter(I2C1);
+	i2c_set_digital_filter(I2C1, 5);
+	i2c_set_speed(I2C1, i2c_speed_sm_100k, 16);
+	i2c_set_7bit_addr_mode(I2C1);
+	i2c_peripheral_enable(I2C1);
 }
 
-static void gpio_setup(void) {
-    gpio_mode_setup(PORT_LED, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, PIN_LED1 | PIN_LED2);
+static void gpio_setup(void)
+{
+	gpio_mode_setup(PORT_LED, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,
+			PIN_LED1 | PIN_LED2);
 
-    gpio_mode_setup(GPIOB, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO8);
+	gpio_mode_setup(GPIOB, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO8);
 }
 
-void uart_out(char* data) {
-    while(*data) {
-        usart_send_blocking(USART1, *data++);
-    }
+void uart_out(char *data)
+{
+	while (*data) {
+		usart_send_blocking(USART1, *data++);
+	}
 }
 
-void delay(int t) {
-    for (int i = 0; i < t; i++) {
-        __asm__("nop");
-    }
+void delay(int t)
+{
+	for (int i = 0; i < t; i++) {
+		__asm__("nop");
+	}
 }
 
 // Fast blink hard fault
-void hard_fault(void) {
-    while(1) {
-        gpio_toggle(PORT_LED, PIN_LED1);
-        delay(1e6);
-    };
+void hard_fault(void)
+{
+	while (1) {
+		gpio_toggle(PORT_LED, PIN_LED1);
+		delay(1e6);
+	};
 }
 
-uint8_t check_faults(uint8_t *fault_counter) {
-    uint8_t ret = 0;
+uint8_t check_faults(uint8_t *fault_counter)
+{
+	uint8_t ret = 0;
 
-    ret = bq76920_read_reg(SYS_STAT);
-    if(ret) {
-        gpio_set(PORT_LED, PIN_LED1);
-        delay(1e6);
-        if(*fault_counter <= 10) {
-            bq76920_clear_faults();
-            bq76920_output_enable();
-            (*fault_counter)++;
-        }
-        else {
-            // Shutdown
-            bq76920_shutdown();
-        }
-    }
-    return ret;
+	ret = bq76920_read_reg(SYS_STAT);
+	if (ret) {
+		gpio_set(PORT_LED, PIN_LED1);
+		delay(1e6);
+		if (*fault_counter <= 10) {
+			bq76920_clear_faults();
+			bq76920_output_enable();
+			(*fault_counter)++;
+		} else {
+			// Shutdown
+			bq76920_shutdown();
+		}
+	}
+	return ret;
 }
 
-int main(void) {
-    uint8_t ret = 0;
-    char buf[128];
-    uint8_t fault_counter = 0;
+int main(void)
+{
+	uint8_t ret = 0;
+	char buf[128];
+	uint8_t fault_counter = 0;
 
-    clock_setup();
-    gpio_setup();
+	clock_setup();
+	gpio_setup();
 
-    gpio_set(PORT_LED, PIN_LED1);
-    delay(1e6);
-    gpio_clear(PORT_LED, PIN_LED1);
+	gpio_set(PORT_LED, PIN_LED1);
+	delay(1e6);
+	gpio_clear(PORT_LED, PIN_LED1);
 
-    usart_setup();
-    i2c_setup();
+	usart_setup();
+	i2c_setup();
 
-    sprintf(buf, "Starting BMS\r\n");
-    uart_out(buf);
+	sprintf(buf, "Starting BMS\r\n");
+	uart_out(buf);
 
-    // Set CC_CFG to 0x19 as per the datasheet
-    bq76920_write_reg(CC_CFG, 0x19);
-    ret = bq76920_read_reg(CC_CFG);
-    if(ret != 0x19) {
-        // Something is wrong with the chip
-        hard_fault();
-    }
+	// Set CC_CFG to 0x19 as per the datasheet
+	bq76920_write_reg(CC_CFG, 0x19);
+	ret = bq76920_read_reg(CC_CFG);
+	if (ret != 0x19) {
+		// Something is wrong with the chip
+		hard_fault();
+	}
 
-    bq76920_init();
-    bq76920_clear_faults();
-    bq76920_output_enable();
+	bq76920_init();
+	bq76920_clear_faults();
+	bq76920_output_enable();
 
-    while(1) {
-        sprintf(buf, "C0: %d C1: %d C2: %d C3: %d C4: %d C5: %d\n\r",
-            bq76920_read_cell_v(0),
-            bq76920_read_cell_v(1),
-            bq76920_read_cell_v(2),
-            bq76920_read_cell_v(3),
-            bq76920_read_cell_v(4),
-            bq76920_read_cell_v(5)
-        );
-        uart_out(buf);
+	while (1) {
+		sprintf(buf, "C0: %d C1: %d C2: %d C3: %d C4: %d C5: %d\n\r",
+			bq76920_read_cell_v(0), bq76920_read_cell_v(1),
+			bq76920_read_cell_v(2), bq76920_read_cell_v(3),
+			bq76920_read_cell_v(4), bq76920_read_cell_v(5));
+		uart_out(buf);
 
-        ret = check_faults(&fault_counter);
-        if(ret) {
-            sprintf(buf, "Fault: %d, Fault Counter: %d\n\r",
-                ret, fault_counter);
-            uart_out(buf);
-        }
+		ret = check_faults(&fault_counter);
+		if (ret) {
+			sprintf(buf, "Fault: %d, Fault Counter: %d\n\r", ret,
+				fault_counter);
+			uart_out(buf);
+		}
 
-        gpio_toggle(PORT_LED, PIN_LED2);
-        delay(5e6);
-    }
+		gpio_toggle(PORT_LED, PIN_LED2);
+		delay(5e6);
+	}
 
-    return 0;
+	return 0;
 }
